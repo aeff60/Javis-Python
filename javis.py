ecbd12834090518e8c0c63ea63c028b8224b5d13
Sirasit0397 <ssbunklnag@gmail.com>
2022-11-23 14:56:49 +0700

    comment all code

--- javis.py
+++ javis.py
@@ -1,72 +1,120 @@
-import pyttsx3
+import pyttsx3 
+
+#pyttsx3 เป็นไลบรารีการแปลงข้อความเป็นคำพูดใน Python 
+#มันทำงานแบบออฟไลน์และเข้ากันได้กับทั้ง Python 2 และ 3
 import speech_recognition as sr
 import datetime
 import os
+import autopy
 
-engine = pyttsx3.init('sapi5')
+# เริ่มต้นใช้งาน engine ชื่อ sapi5
+# sapi5 เทคโนโลยีสำหรับ voice recognition และ synthesis โดย Microsoft
+# voice recognition คือ การแปลงเสียงเป็นข้อความ
+# synthesis คือ การแปลงข้อความเป็นเสียง
+# ในกรณีนี้เราจะใช้เพื่อให้เสียงออกมา
+
+engine = pyttsx3.init('sapi5') 
+# ให้เสียงพูดว่า "สวัสดีครับ"
+speak("สวัสดีครับ")
+
+# รับลิสต์เสียงสำหรับ engine
+#  engine.getProperty คือ แสดงรายชื่อเสียงที่สามารถใช้งานได้
 voices = engine.getProperty('voices')
+
+# เลือกเสียงแรกที่ใช้ได้ จาก voices[0] 0 คือตำแหน่งที่ 1
 engine.setProperty('voice', voices[0].id)
 
-def speak(audio):
-    engine.say(audio)
-    engine.runAndWait()
+#ฟังก์ชั่นในการพูด
+def speak(audio): 
+    engine.say(audio) #พูดตามข้อมูลที่อยู่ในตัวแปล audio
+    engine.runAndWait() #รอให้เสียงเล่นจบ
 
 def commands():
-    while True:
-        r=sr.Recognizer()
-        with sr.Microphone() as source:
-            print("Listening...")
-            r.pause_threshold = 1
-            r.adjust_for_ambient_noise(source, duration=1)
-            audio = r.listen(source)
+    
 
-        try:
-            print("Recognizing...")
-            query = r.recognize_google(audio, language='th-TH')
-            print(f"User said: {query}\n")
+    r=sr.Recognizer() # สร้างตัวแปร r เพื่อรับค่าจากการฟัง 
+      
+    with sr.Microphone() as source: # ใช้ไมโครโฟนเป็น source ในการรับเสียง 
+        print("Listening...")
+        # ปรับความไวของตัวจำแนกตามเสียงรบกวนรอบข้างและบันทึกเสียง
+        r.pause_threshold = 1 # หยุดรอคำสั่ง 1 วินาที
+        r.adjust_for_ambient_noise(source, duration=1) # ปรับระดับเสียงรบกวน 1 วินาที 
+        audio = r.listen(source) # รับเสียงจากไมโครโฟน
 
-        except Exception as e:
-            print(e)
-            print("Say that again please...")
-            return "None"
-        return query
+    try: # ถ้ารับเสียงได้
+        print("Recognizing...")
+        # อินสแตนซ์ของ Recognizer โดยใช้ Google Speech Recognition AP
+        query = r.recognize_google(audio, language='th-TH') # ภาษาไทย 
 
+        print(f"User said: {query}\n")# แสดงข้อความที่เราพูด ในคอนโซล
+
+    except Exception as e: # ถ้าไม่มีการพูด
+        print(e) # แสดงข้อความ error 
+        print("Say that again please...")# แสดงข้อความ พูดใหม่ 
+        return "None"# คืนค่า None 
+    return query# คืนค่า query
+
+# ฟังก์ชั่นนี้ต้องการให้ผู้ใช้ตามเวลาของวัน
 def wishings():
+    # รับชั่วโมงปัจจุบัน
     hour = int(datetime.datetime.now().hour)
+    # ถ้าชั่วโมงอยู่ระหว่าง 0 ถึง 12, ให้พูดว่า Good Morning
     if hour>=0 and hour<12:
         speak("Good Morning!")
 
+    # ถ้าชั่วโมงอยู่ระหว่าง 12 ถึง 18, ให้พูดว่า Good Afternoon
     elif hour>=12 and hour<18:
         speak("Good Afternoon!")   
 
+    # มิฉะนั้น, ให้พูดว่า Good Evening
     else:
         speak("Good Evening!")  
 
-    speak("I am Jarvis Sir. Please tell me how may I help you")
+    #สุดท้ายนี้ขอให้ผู้ใช้ พูดว่า สวัสดี และ ชื่อของผู้ใช้
+
+    speak("สวัสดี")
 
 
 if __name__ == "__main__":
 
-    wishings()
-    query = commands().lower()
+    
     while True:
-        if 'time' in query:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+        wishings()# ให้เรียกใช้ฟังก์ชั่น wishings
+        query = commands().lower()# รับคำสั่งจากฟังก์ชั่น commands และเปลี่ยนเป็นตัวพิมพ์เล็กทั้งหมด
+        if 'time' in query:  
+         
+            # ใช้เมธอด now() เพื่อรับเวลาปัจจุบันมาและเมธอด strftime() เพื่อทำให้เวลาอยู่ในรูปแแบบข้อความ
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            # ใช้ฟังก์ชัน speak() เพื่อพูดเวลา
             speak(f"Sir, the time is {strTime}") 
 
+        elif 'comment' in query:
+            # typing sharp with autopy
+            autopy.key.tap('#')
+            query = "null"
+            speak("ได้เลยครับ พิมพ์คอมเม้นต์ของคุณ")
+
         elif 'open firefox' in query:
+            # ใช้ฟังก์ชัน speak() เพื่อพูดว่า กำลังเปิด Firefox
             speak("Opening Firefox")
+            # ใช้ฟังก์ชัน webbrowser.open() เพื่อเปิด Firefox
             os.startfile("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe")
 
         elif 'wikipedia' in query:
+            # ใช้ฟังก์ชัน speak() เพื่อพูดว่า กำลังค้นหาใน Wikipedia
             speak("Searching Wikipedia...")
             try:
+                # ใช้ฟังก์ชัน query.replace() เพื่อเปลี่ยนคำว่า wikipedia เป็นค่าว่าง
                 query = query.replace("wikipedia", "")
+                # ใช้ฟังก์ชัน results เพื่อค้นหาคำที่ผู้ใช้พูดใน Wikipedia
                 results = wikipedia.summary(query, sentences=2)
+                # ใช้ฟังก์ชัน speak() เพื่อพูดผลลัพธ์
                 speak("According to Wikipedia")
+                # ใช้ฟังก์ชัน speak() เพื่อพูดผลลัพธ์
                 print(results)
                 speak(results)
             except:
+                # ใช้ฟังก์ชัน speak() เพื่อพูดว่า ไม่พบผลลัพธ์
                 speak("Sorry, I can't find it")
                 print("Sorry, I can't find it")
 
